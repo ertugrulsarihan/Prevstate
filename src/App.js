b7990cf0e94c06414626d490d10bf59d82b814c0
ertugrulsarihan <ertugrulsarihan1@gmail.com>
2021-10-31 23:26:26 +0300

    İnitial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dummy2, setDummy2] = useState({ name: "", surname: "" });
+
+  const [dummyArray, setArray] = useState([]);
+  const [lastValue, setLastValue] = useState({});
+  const increaseToDummy2 = () => {
+    setArray([...dummyArray, dummy2]);
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setDummy2((prevstate) => {
+      let newDummy = Object.assign({}, prevstate);
+      newDummy[name] = value;
+      return newDummy;
+    });
+  };
+
+  const deleteLast = () => {
+    setArray((prevstate) => {
+      let newArray = Object.assign([], prevstate);
+      if (prevstate.length !== 1) newArray.pop();
+      return newArray;
+    });
+  };
+
+  useEffect(() => {
+    setArray(dummyArray);
+    setLastValue(dummyArray[dummyArray.length - 1]);
+  }, [dummyArray]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <input
+        onChange={(e) => handleChange(e)}
+        placeholder="Name giriniz"
+        className="form-control"
+        name="name"
+        type="text"
+      />
+      <input
+        onChange={(e) => handleChange(e)}
+        placeholder="Surname giriniz"
+        className="form-control"
+        name="surname"
+        type="text"
+      />
+      {dummyArray.map((d) => (
+        <ul>
+          <li>{`${d.name} ${d.surname}`}</li>
+        </ul>
+      ))}
+      <button className="btn btn-primary" onClick={increaseToDummy2}>
+        Arttır
+      </button>
+      <button onClick={deleteLast}>Delete</button>
     </div>
   );
 }
